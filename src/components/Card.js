77fa3882ef61bfa67ffe4900a269a2ee06ef2d73
ATIKSH09 <atikshgupta6373@gmail.com>
2024-09-08 16:43:06 +0530

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Card({ link, title, category, price, off }) {
   return (
-    <div className="w-72 rounded-lg shadow-lg hover:shadow-2xl  " data-aos="fade-up">
-      <img src={link} alt="" />
+    <div className="w-72    " data-aos="fade-up">
+      <img className="hover:shadow-2xl" src={link} alt="" />
       <h1 className="  font-semibold  border-black pt-2 border-b">{title}</h1>
       <h1 className="  text-xs ">{category}</h1>
       <div className=" font-bold flex justify-between pt-2 text-2xl">
